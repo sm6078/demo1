4b91153132a17bb973f04ea42281a47a4f4e7183
Sergei Rodin <sm6078@mail.ru>
2025-07-26 12:42:05 +0300

    работа с пеменными 1

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Привет")
+	var userHeigth = 1.85
+	var userKg float64 = 114
+	// var IMT = float64(userKg) / math.Pow(userHeigth, 2)
+	var IMT = userKg / math.Pow(userHeigth, 2)
+	fmt.Print(IMT)
 }
