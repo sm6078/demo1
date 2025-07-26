ff1e4668630222372858d23e2a32f58156590cec
Sergei Rodin <sm6078@mail.ru>
2025-07-26 13:49:32 +0300

    работа с консолью, ввод - вывод

--- main.go
+++ main.go
@@ -6,30 +6,16 @@ import (
 )
 
 func main() {
-	//короткая запись
-	//var userHeigth, userKg float64 = 1.85, 100
-
-	//еще более короткая запись, но не можем указать тип переменной придется делать явное преобразование при расчете
-	//userHeigth, userKg := 1.85, 100
-
-	//без var
-	//userHeigth := 1.85
-
-	//можно задать переменную, а потом писвоить ей значение
-	//var userKg float64
-	//userKg = 114
-
-	//Константы
-	//при объявлении константы можем явно указывать тип
-	//const IMTPower int = 2
-
-	//при объявлении константы можем явно не указывать тип - он буде untyped
 	const IMTPower = 2
 
-	//можно сделать вот так
-	userHeigth := 1.85
-	userKg := 100.0
+	var userKg, userHeigth float64
 
+	fmt.Print("___Калькулятор индекса массы тела__\n")
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeigth)
+	fmt.Print("Введите свой вес в килограммах: ")
+	fmt.Scan(&userKg)
 	IMT := userKg / math.Pow(userHeigth, IMTPower)
+	fmt.Print("Ваш индекс массы тела: ")
 	fmt.Print(IMT)
 }
