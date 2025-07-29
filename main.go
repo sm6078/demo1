edcabf3c9678df18ff7808f121bca3c43292431d
Sergei Rodin <sm6078@mail.ru>
2025-07-29 11:35:41 +0300

    форматирование строк

--- main.go
+++ main.go
@@ -10,12 +10,18 @@ func main() {
 
 	var userKg, userHeigth float64
 
-	fmt.Print("___Калькулятор индекса массы тела__\n")
-	fmt.Print("Введите свой рост в метрах: ")
+	//fmt.Print("___Калькулятор индекса массы тела__\n")
+	fmt.Println("___Калькулятор индекса массы тела__")
+	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeigth)
 	fmt.Print("Введите свой вес в килограммах: ")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeigth, IMTPower)
-	fmt.Print("Ваш индекс массы тела: ")
-	fmt.Print(IMT)
+	IMT := userKg / math.Pow(userHeigth/100, IMTPower)
+	//fmt.Print("Ваш индекс массы тела: ")
+	//fmt.Print(IMT)
+	//--вывод строки с переменной
+	//fmt.Printf("Ваш индекс массы тела: %v", IMT)
+	//форматирование строки 0 символов после точки с округлением
+	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
+	//fmt.Printf("Ваш индекс массы тела: %.2f", IMT)
 }
