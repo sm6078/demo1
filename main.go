7d5d0d3cda96173cafd9a3a30f01d955d908366b
Sergei Rodin <sm6078@mail.ru>
2025-08-03 01:20:29 +0300

    функции - функция, которая возвращает несколько параметров

--- main.go
+++ main.go
@@ -6,17 +6,14 @@ import (
 )
 
 func main() {
-	var userKg, userHeigth float64
 
 	//fmt.Print("___Калькулятор индекса массы тела__\n")
 	//fmt.Println("___Калькулятор индекса массы тела__")
 	//fmt.Print("Введите свой рост в сантиметрах: ")
 	//-мультистроки - приется убрать все табы, нарушить кодстайл
-	fmt.Print(`___Калькулятор индекса массы тела__
-Введите свой рост в сантиметрах: `)
-	fmt.Scan(&userHeigth)
-	fmt.Print("Введите свой вес в килограммах: ")
-	fmt.Scan(&userKg)
+	fmt.Println("___Калькулятор индекса массы тела__")
+	userHeigth, userKg := getUserInput()
+
 	IMT := calculateIMT(userKg, userHeigth)
 	//вызов созданной функции в рамках одного пакета
 	outputResult(IMT)
@@ -33,4 +30,16 @@ func calculateIMT(userKg float64, userHeigth float64) float64 {
 	const IMTPower = 2
 	IMT := userKg / math.Pow(userHeigth/100, IMTPower)
 	return IMT
+}
+
+//функция, которая возвращает несколько параметров
+
+func getUserInput() (float64, float64) {
+	var userKg float64
+	var userHeigth float64
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeigth)
+	fmt.Print("Введите свой вес в килограммах: ")
+	fmt.Scan(&userKg)
+	return userHeigth, userKg
 }
